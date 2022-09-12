da321e922d379247d657ee573ccd7ce556b8b11f
Swarda Joshi <swarda_joshi@persistent.com>
2022-09-12 19:23:02 +0530

    react assignment 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'; //imported usestate
 
-function App() {
+const Timer = () =>{                 //Arrow Function
+
+  const [seconds,timerseconds]=useState(0);       //usestate hook [current state, update state]
+  const [minutes,timerminutes]=useState(0); 
+  const [hours,timerhours]=useState(0);       //usestate hook [current state, update state]
+
+
+var timer;
+
+useEffect(()=>{                   //call function repeatedly, actions after rendering
+timer=setInterval(()=>{
+  timerseconds(seconds+1) 
+  if (seconds===59)
+  {
+    timerminutes(timerminutes+1)
+    timerseconds(0);
+    
+  } 
+  if (minutes===59)
+  {
+    timerhours(timerhours+1)
+    timerminutes(0);
+    timerseconds(0);
+
+  }     
+       //seconds increment
+},1000)
+
+return () => clearInterval (timer);//call after 1000ms})
+
+});
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Timeapp">
+      <h1>Timer App</h1>
+      <input type="number"></input>
+       <h1 >Hours: Minutes : Seconds </h1>
+       <h1 >{hours} : {minutes}: {seconds}</h1>
+      
     </div>
   );
+  
 }
 
-export default App;
+export default Timer;
+  
