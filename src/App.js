9b368ffcbc26dfcf8d966c9a0b06a78087bfb057
Swarda Joshi <swarda_joshi@persistent.com>
2022-09-13 17:53:04 +0530

    added css and functionalities to button

--- src/App.js
+++ src/App.js
@@ -1,48 +1,125 @@
 import { useEffect, useState } from 'react'; //imported usestate
+import './App.css';
 
 const Timer = () =>{                 //Arrow Function
 
+
   const [seconds,timerseconds]=useState(0);       //usestate hook [current state, update state]
   const [minutes,timerminutes]=useState(0); 
-  const [hours,timerhours]=useState(0);       //usestate hook [current state, update state]
-
-
+  const [hours,timerhours]=useState(0);      
+  
+  
 var timer;
 
-useEffect(()=>{                   //call function repeatedly, actions after rendering
-timer=setInterval(()=>{
-  timerseconds(seconds+1) 
-  if (seconds===59)
+useEffect(()=>{   
+  
+  if (seconds>60 || minutes>60 || hours>24)//validations
   {
-    timerminutes(timerminutes+1)
-    timerseconds(0);
-    
-  } 
-  if (minutes===59)
-  {
-    timerhours(timerhours+1)
-    timerminutes(0);
-    timerseconds(0);
 
-  }     
-       //seconds increment
+    timerseconds(0)
+    timerminutes(0)
+    timerhours(0)
+    return (alert("Wrong Input!"))
+    
+  }  
+  
+  if (seconds===0 && minutes===0 && hours===0)//timeout
+{
+  
+  return (alert("Timeout Completed!"))
+  
+  
+}               
+timer=setInterval(()=>{            //call function repeatedly, actions after rendering
+timerseconds(seconds-1) 
+
+if (seconds===0 && minutes>0 && hours>0)
+{
+  timerseconds(59)
+  timerminutes(minutes-1)
+ 
+}
+
+if (seconds===0 && minutes===0 && hours>=0)
+{
+  timerseconds(59)
+  timerminutes(59)
+  timerhours(hours-1)
+}
+
+if (seconds===0 && minutes>0)
+{
+  timerseconds(59)
+  timerminutes(minutes-1)
+}
+
+if (seconds<0 || minutes<0 || hours<0)
+{
+    timerseconds(0)
+    timerminutes(0)
+    timerhours(0)
+    return (alert("Wrong Input!"))
+}
+
+
+ 
 },1000)
 
 return () => clearInterval (timer);//call after 1000ms})
 
 });
   
+
+  
+const reset =()=>{
+   
+  timerseconds(0)
+  timerminutes(0)
+  timerhours(0)
+   
+  }
+
+
+const stop =()=>{
+   
+ clearInterval(timer); 
+ 
+  
+ }
+ const play =()=>{
+   
+  timerseconds(seconds)
+  timerminutes(minutes)
+  timerhours(hours)
+   
+  }
+
+  
+
   return (
+    
+    
     <div className="Timeapp">
       <h1>Timer App</h1>
-      <input type="number"></input>
-       <h1 >Hours: Minutes : Seconds </h1>
-       <h1 >{hours} : {minutes}: {seconds}</h1>
+      <div className="Inputs">
+      <input onChange={(e) => {timerhours(e.target.value);}} placeholder='Enter Hours'/>
+            <input onChange={(e) => {timerminutes(e.target.value);}} placeholder='Enter Minutes'/>
+            <input onChange={(e) => {timerseconds(e.target.value);}} placeholder='Enter Seconds'/>
+            </div>
+           
+      <h1>{hours} : {minutes} : {seconds}</h1>
+    
+      <div className="Button">
+      <button className='reset' onClick={reset}>Reset</button>
+      <button className='play' onClick={play}>Play</button>
+      <button className='stop' onClick={stop}>Stop</button>
+      </div>
       
     </div>
   );
   
 }
+
 
 export default Timer;
   
